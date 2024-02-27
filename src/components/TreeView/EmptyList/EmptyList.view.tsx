f1a8d2c67bbc2d0785d70a6f2c2daaa7668bc9e6
farahyasserr <farah.dawoudd@gmail.com>
2024-02-27 15:52:50 +0400

    feat(emptyListView): Create Basic Empty List Component

---
+++ src/components/TreeView/EmptyList/EmptyList.view.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function EmptyList({}) {
+  return (
+    <View>
+      <Text>Product List is empty.</Text>
+    </View>
+  );
+}
+
+export default EmptyList;
