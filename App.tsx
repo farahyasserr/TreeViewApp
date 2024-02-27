6045094b7d8d7b9e7200360ded7dc08a2f8122d5
farahyasserr <farah.dawoudd@gmail.com>
2024-02-28 01:31:53 +0400

    feat(ui): Safe Area View styling

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
 
 import HomeScreen from './src/screens';
+import {colors} from './src/theme/Colors';
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.safeAreaView}>
       <HomeScreen />
     </SafeAreaView>
   );
 }
 
+const styles = StyleSheet.create({
+  safeAreaView: {flex: 1, backgroundColor: colors.background},
+});
 export default App;
